166582b62abcce7210d42b0145dd66e3c3983ca7
Alex Jilkin <jilkinalex@gmail.com>
2023-02-04 13:10:52 +0200

    Add score that increases every second

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text scoreText;
+    private int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("IncreaseScoreEnum");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator IncreaseScoreEnum()
+    {
+        while (true)
+        {
+           yield return new WaitForSeconds(1);
+           this.IncreaseScore();
+        }
+
+        yield return null;
+    }
+
+    private void IncreaseScore() {
+        this.score++;
+        this.scoreText.text = $"Score: {this.score}";
+    }
+}
