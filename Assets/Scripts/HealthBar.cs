283731aed32eb3e8be5d82eaba514535f4fd2746
Ebema0 <ebema001@edu.xamk.fi>
2023-02-04 17:47:54 +0200

    Create HealthBar.cs

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Example video is https://www.youtube.com/watch?v=BLfNP4Sc_iA
+public class HealthBar : MonoBehaviour
+
+
+{
+    // Start is called before the first frame update
+    public Gradient gradient;
+    public Image fill;
+    public Slider slider;
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+        fill.color = gradient.Evaluate() ;  
+    }
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+
+    }
+
+        
+
+}
